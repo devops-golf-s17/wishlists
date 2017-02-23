95a325de41aec0d37570e5d5d295769a854776c2
Jesse Gumz <jdg513@nyu.edu>
2017-02-23 00:46:31 -0500

    add self params, complete basic implementation

--- persistence/wishlist.py
+++ persistence/wishlist.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import json
 from datetime import datetime
-
-from item import Item
 
 
 class Wishlist(object):
@@ -12,9 +11,10 @@ class Wishlist(object):
     an individual "wishlist resource".
     """
 
-    UPDATABLE_FIELDS = ['name']
+    UPDATABLE_WISHLIST_FIELDS = ['name']
+    UPDATABLE_ITEM_FIELDS = ['description']
 
-    def __init__():
+    def __init__(self):
         """
         Creates a singleton Wishlist object that manages all
         Wishlist resources.
@@ -26,7 +26,7 @@ class Wishlist(object):
         # every time a new one is added, the index count goes up like it would in a database
         self._index = 0
 
-    def create_wishlist(name, user_id):
+    def create_wishlist(self, name, user_id):
         """
         Make a new wishlist resource with a specified name and user.
         Assuming everything is created OK, return the resource's data
@@ -40,14 +40,15 @@ class Wishlist(object):
         new_wishlist['name'] = name
         new_wishlist['user_id'] = user_id
         new_wishlist['items'] = {}
-        new_wishlist['created'] = datetime.utcnow()
+        # get a nicely formatted, human-readable datetime
+        new_wishlist['created'] = datetime.utcnow().__repr__()
         new_wishlist['deleted'] = False
         self._index += 1
         self._wishlist_resources[self._index] = new_wishlist
 
-        return new_wishlist
+        return json.dumps(new_wishlist)
 
-    def _verify_wishlist_exists(wishlist_id):
+    def _verify_wishlist_exists(self, wishlist_id):
         """
         A quick check that returns True if the wishlist exists and has not
         been deleted.  Otherwise, returns False.
@@ -61,7 +62,7 @@ class Wishlist(object):
 
         return False
 
-    def delete_wishlist(wishlist_id):
+    def delete_wishlist(self, wishlist_id):
         """
         Soft-delete a wishlist resource based on a provided id.
         The soft deletion is implemented as setting the "deleted" field to False.
@@ -70,8 +71,8 @@ class Wishlist(object):
         """
 
         try:
-            # even if a delete wishlist call was already made, this will just set the value to False again
-            self._wishlist_resources[wishlist_id]['deleted'] = False
+            # even if a delete wishlist call was already made, this will just set the value to True again
+            self._wishlist_resources[wishlist_id]['deleted'] = True
         except KeyError:
             # cannot delete something that did not exist beforehand
             raise WishlistNotFoundException
@@ -93,12 +94,19 @@ class Wishlist(object):
         item_description = item_data.get('description')
 
         if self._verify_wishlist_exists(wishlist_id):
-            self._wishlist_resources[wishlist_id]['items'][item_id] = {'description': item_description}
-            return {'id': item_id, 'description': item_description}
+            if item_id in self._wishlist_resources[wishlist_id][item_id]:
+                # one cannot add an item that already exists
+                # note: although it would not be an issue to merely overwrite the data,
+                # that would not be a proper result of a POST request to add an item
+                raise WishlistOperationNotPermittedException
+            else:
+                # add a new item
+                self._wishlist_resources[wishlist_id]['items'][item_id] = {'description': item_description}
+                return {'id': item_id, 'description': item_description}
         else:
             raise WishlistNotFoundException
 
-    def remove_item(wishlist_id, item_id):
+    def remove_item(self, wishlist_id, item_id):
         """
         Given a wishlist_id and item_id, remove the item in the wishlist
         that matches up with the item_id
@@ -115,12 +123,16 @@ class Wishlist(object):
             except KeyError:
                 raise WishlistItemNotFoundException
         else:
-            # the wishlist does not exist at all
+            # the wishlist does not exist or has been deleted
             raise WishlistNotFoundException
 
-    def update_wishlist(wishlist_id, **kwargs):
+    def update_wishlist(self, wishlist_id, **kwargs):
         """
-        Accepts a dictionary of terms and then loops through them to see
+        NOTE: You must pass in a dictionary with "**" in front of it for the kwargs argument.
+        As of right now this is probably excessive since we only have one field that can be
+        updated, but it will be useful later on.
+
+        Accepts a wishlist_id and a dictionary of terms and then loops through them to see
         if any match up with the updateable fields specified in the class.
         If there is a match (e.g., kwargs.get('name') has a value), then
         the inputted value overwrites the existing value (and hence 
@@ -131,17 +143,83 @@ class Wishlist(object):
 
         :return: <dict> the modified wishlist resource
         """
+        if self._verify_wishlist_exists(wishlist_id):
+            for key in kwargs:
+                if key in Wishlist.UPDATABLE_WISHLIST_FIELDS:
+                    # OK to update the field
+                    self._wishlist_resources[wishlist_id][key] = kwargs.get(key)
 
-        for key in kwargs:
-            if key in Wishlist.UPDATABLE_FIELDS:
-                # OK to update the field
-                self._wishlist_resources[wishlist_id][key] = kwargs.get(key)
+            # return the modified resource
+            return self._wishlist_resources[wishlist_id]
+        else:
+            raise WishlistNotFoundException
 
-        # return the modified resource
-        return self._wishlist_resources[wishlist_id]
+    def update_wishlist_item(self, wishlist_id, item_id, **kwargs):
+        """
+        NOTE: You must pass in a dictionary with "**" in front of it for the kwargs argument.
+        As of right now this is probably excessive since we only have one field that can be
+        updated, but it will be useful later on.
 
+        Accepts a wishlist_id, item_id, and a dictioanry of terms before
+        looping through them to see if any match up with the updateable fields
+        specified in the class. If there is a match (e.g., kwargs.get('name') has a value),
+        then the inputted value overwrites the existing value
+        (and hence performs an update).
 
+        :param wishlist_id: <int> the id of the wishlist to which the item to be modified belongs
+        :param item_id: <str> the id of the item to be modified
+        :param kwargs: <dict> a variable number of key/value pairs that may contain fields to be updated
 
+        :return: <dict> the modified wishlist resource
+        """
+
+        if self._verify_wishlist_exists(wishlist_id):
+            try:
+                for key in kwargs:
+                    if key in Wishlist.UPDATABLE_ITEM_FIELDS:
+                        # OK to update the item
+                        self._wishlist_resources[wishlist_id][item_id][key] = kwargs.get(key)
+            except KeyError:
+                # the item did not exist
+                raise WishlistItemNotFoundException
+
+            # return the modified resource
+            return self._wishlist_resources[wishlist_id]
+        else:
+            raise WishlistNotFoundException
+
+    def retrieve_wishlist_resource(self, wishlist_id):
+        """
+        Given a specific wishlist_id, return the corresponding resource or raise
+        an exception if it cannot be found.
+
+        :param wishlist_id: <str> the id for the wishlist to be returned
+        """
+
+        if self._verify_exists(wishlist_id):
+            return json.dumps(self._wishlist_resources[wishlist_id])
+        else:
+            raise WishlistNotFoundException
+
+    def retrieve_all_wishlist_resources(self, include_deleted=False):
+        """
+        Returns a list of all existing wishlist resources.  Note that
+        deleted resources are *not* included by default; this flag must
+        be explicitly set by passing in include_deleted=True.
+
+        :param include_deleted: <bool> whether or not to include soft-deleted wishlists in the returned list
+        """
+
+        all_wishlists = []
+
+        if include_deleted:
+            # use a list comprehension to easily retrieve the dictionaries and merge them together into a JSON string
+            all_wishlists = [json.dumps({key: contents}) for key, contents in self._wishlist_resources.iteritems()]
+        else:
+            # filter out those key, contents pairs where contents['deleted'] == True 
+            all_wishlists = [json.dumps({key: contents}) for key, contents in self._wishlist_resources.iteritems() if contents['deleted'] == False]
+
+        return all_wishlists
 
 
 class WishlistException(Exception):
@@ -154,3 +232,7 @@ class WishlistNotFoundException(WishlistException):
 
 class WishlistItemNotFoundException(WishlistException):
     """ Raised when a wishlist item is unable to be retrieved """
+
+
+class WishlistOperationNotPermittedException(WishlistException):
+    """ Raised when an operation is deemed to be unacceptable """
