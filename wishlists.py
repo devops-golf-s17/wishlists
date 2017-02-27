d379b1bbe5df972053a5b7a969147b7062fa56fd
Jesse Gumz <jdg513@nyu.edu>
2017-02-26 20:57:58 -0500

    initial commit, add support for reading item resources

--- wishlists.py
+++ wishlists.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, Response, jsonify, request, json
 
 from persistence import db
+from persistence.persistence import WishlistNotFoundException, ItemNotFoundException
 
 # Create Flask application
 app = Flask(__name__)
@@ -21,6 +22,37 @@ def index():
     wishlist_url = request.base_url + 'wishlists'
     return (jsonify(service='wishlists', version='0.1',
             url=wishlist_url), HTTP_200_OK)
+
+
+@app.route('/wishlists/<int:wishlist_id>/items', methods=['GET'])
+def item(wishlist_id):
+    """
+    The route for getting all items associated with a wishlist
+    or making a new item for a wishlist via a POST.
+    """
+
+    if request.method == 'GET':
+        try:
+            items = db.retrieve_all_items(wishlist_id)
+            return items, HTTP_200_OK
+        except WishlistException:
+            return 'Could not find a wishlist with id %s' % wishlist_id, HTTP_404_NOT_FOUND
+
+
+@app.route('/wishlists/<int:wishlist_id>/items/<string:item_id>', methods=['GET'])
+def read_wishlist_item(wishlist_id, item_id):
+    """
+    The route for retrieving a specific item in a wishlist.
+    """
+    
+    try:
+        item = db.retrieve_item(wishlist_id, item_id)
+        return item, HTTP_200_OK
+    except ItemNotFoundException:
+        return 'Item with id %s could not be found' % item_id, HTTP_404_NOT_FOUND
+    except WishlistNotFoundException:
+        return 'Wishlist with id %d could not be found' % wishlist_id, HTTP_404_NOT_FOUND
+
 
 if __name__ == '__main__':
 
