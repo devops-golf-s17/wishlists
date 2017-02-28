200248e2023b6542498392c209e35a1262a3ae52
Ankit Bhagat <ankit.bhagat@live.com>
2017-02-27 20:47:26 -0500

    Removed the automatic creation of a wishlist.

--- wishlists.py
+++ wishlists.py
@@ -17,7 +17,7 @@ HTTP_400_BAD_REQUEST = 400
 HTTP_404_NOT_FOUND = 404
 HTTP_409_CONFLICT = 409
 
-db.create_wishlist('jesse','test')
+#db.create_wishlist('jesse','test')
 
 @app.route('/')
 def index():
