f4c117e4a3544468b915db9fa65d4bbb336af3b4
Palak Bhasin <palak.bhasin@hotmail.com>
2017-03-29 19:01:19 -0400

    Add test file

---
+++ tests/test_wishlists.py
@@ -0,0 +1,17 @@
+
+# Test cases can be run with either of the following:
+# python -m unittest discover
+# nosetests -v --rednose --nologcapture
+
+import unittest
+from persistence import DatabaseEngine
+
+class WishlistTestCase(unittest.TestCase):
+  def setUp(self):
+    self.db=DatabaseEngine()
+    
+  def tearDown(self):
+    self.db=None
+    
+if __name__ == '__main__':
+  unittest.main()
