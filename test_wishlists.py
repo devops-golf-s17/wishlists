550b4905e55fdaf86ce6bdc319bc62c9254080c7
Palak Bhasin <palak.bhasin@hotmail.com>
2017-04-01 15:08:45 -0400

    added unit tests for updating items and clearing a wishlist

--- test_wishlists.py
+++ test_wishlists.py
@@ -1,6 +1,10 @@
  #Test cases can be run with either of the following:
  #python -m unittest discover
  #nosetests -v --rednose --nologcapture
+ #nosetests --verbosity 2 --with-spec --spec-color
+ #To check coverage:
+ #coverage run --omit "venv/*" test_wishlists.py
+ #coverage report -m --include= wishlists.py
 
 import json
 import unittest
@@ -21,7 +25,6 @@ class WishlistTestCase(unittest.TestCase):
 
 
 	"""
-		Working test case.
 		This is a test case to check whether a wishlist is created or not.
 		POST verb checked here.
 	"""
@@ -37,7 +40,6 @@ class WishlistTestCase(unittest.TestCase):
 		self.assertEqual(len(all_wishlists_json),2)
 
 	"""
-		Working test case.
 		This is a test case to check whether an item is added to a wishlist or not.
 		POST verb is checked here.
 	"""
@@ -55,7 +57,6 @@ class WishlistTestCase(unittest.TestCase):
 		
 
 	"""
-		Not working test case.
 		This is a test case to check whether an item is added to a wishlist out of index.
 		POST verb is checked here.
 	"""
@@ -64,6 +65,11 @@ class WishlistTestCase(unittest.TestCase):
 		data = json.dumps(new_item)
 		resp = self.app.post('/wishlists/3/items',data=data,content_type='application/json')
 		self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+	"""
+		This is a test case to check whether a wishlist is updated.
+		PUT verb checked here.
+	"""
 
 	def test_update_wishlist(self):
 		new_wl = {'name': 'wl2', 'user_id': 'user2'}
@@ -74,13 +80,28 @@ class WishlistTestCase(unittest.TestCase):
 		self.assertEqual (new_json['name'], 'wl2')
 		self.assertEqual (new_json['user_id'], 'user2')
 
+	"""
+		This is a test case to check whether an error is returned when no data is sent for a wishlist.
+		PUT verb checked here.
+	"""
+
 	def test_update_wishlist_with_no_data(self):
 		resp = self.app.put('/wishlists/1', data=None, content_type='application/json')
 		self.assertEqual( resp.status_code, status.HTTP_400_BAD_REQUEST )
 
+	"""
+		This is a test case to check whether an error is returned when the incorrect data format is sent for a wishlist.
+		PUT verb checked here.
+	"""
+
 	def test_update_wishlist_with_text_data(self):
 		resp = self.app.put('/wishlists/1', data="hello", content_type='text/plain')
 		self.assertEqual( resp.status_code, status.HTTP_400_BAD_REQUEST )
+
+	"""
+		This is a test case to check whether an error is returned when the insufficient data is sent for a wishlist.
+		PUT verb checked here.
+	"""
 
 	def test_update_wishlist_with_no_name(self):
 		new_wl = {'user_id': 'user2'}
@@ -88,16 +109,110 @@ class WishlistTestCase(unittest.TestCase):
 		resp = self.app.put('/wishlists/1', data=data, content_type='application/json')
 		self.assertEqual( resp.status_code, status.HTTP_400_BAD_REQUEST )
 
+	"""
+		This is a test case to check whether an error is returned when the insufficient data is sent for a wishlist.
+		PUT verb checked here.
+	"""
+
 	def test_update_wishlist_with_no_user_ID(self):
 		new_wl = {'name': 'wl2'}
 		data = json.dumps(new_wl)
 		resp = self.app.put('/wishlists/1', data=data, content_type='application/json')
 		self.assertEqual( resp.status_code, status.HTTP_400_BAD_REQUEST )
 
+	"""
+		This is a test case to check whether an error is returned when a nonexistent wishlist is updated.
+		PUT verb checked here.
+	"""
+
 	def test_update_wishlist_not_found(self):
 		new_wl = {'name': 'wl2', 'user_id': 'user2'}
 		data = json.dumps(new_wl)
 		resp = self.app.put('/wishlists/2', data=data, content_type='application/json')
+		self.assertEqual( resp.status_code, status.HTTP_404_NOT_FOUND )
+
+	"""
+		This is a test case to check whether an item is updated.
+		PUT verb checked here.
+	"""
+
+	def test_update_item(self):
+		new_item = {'description': 'test update'}
+		data = json.dumps(new_item)
+		resp = self.app.put('/wishlists/1/items/item1', data=data, content_type='application/json')
+		self.assertEqual( resp.status_code, status.HTTP_200_OK )
+		new_json = json.loads(resp.data)
+		self.assertEqual (new_json['items']['item1']['description'], 'test update')
+
+	"""
+		This is a test case to check whether an error is returned when empty data is sent for an item.
+		PUT verb checked here.
+	"""
+
+	def test_update_item_with_no_data(self):
+		resp = self.app.put('/wishlists/1/items/item1', data=None, content_type='application/json')
+		self.assertEqual( resp.status_code, status.HTTP_400_BAD_REQUEST )
+
+	"""
+		This is a test case to check whether an error is returned when the incorrect data format is sent for an item.
+		PUT verb checked here.
+	"""
+
+	def test_update_item_with_text_data(self):
+		resp = self.app.put('/wishlists/1/items/item1', data="hello", content_type='text/plain')
+		self.assertEqual( resp.status_code, status.HTTP_400_BAD_REQUEST )
+
+	"""
+		This is a test case to check whether an error is returned when the insufficient data is sent for an item.
+		PUT verb checked here.
+	"""
+
+	def test_update_item_with_no_description(self):
+		new_item = {'id':'item1'}
+		data = json.dumps(new_item)
+		resp = self.app.put('/wishlists/1/items/item1', data=data, content_type='application/json')
+		self.assertEqual( resp.status_code, status.HTTP_400_BAD_REQUEST )
+
+	"""
+		This is a test case to check whether an error is returned when a nonexistent item is updated.
+		PUT verb checked here.
+	"""
+
+	def test_update_item_not_found(self):
+		new_item = {'description': 'test update'}
+		data = json.dumps(new_item)
+		resp = self.app.put('/wishlists/1/items/item2', data=data, content_type='application/json')
+		self.assertEqual( resp.status_code, status.HTTP_404_NOT_FOUND )
+
+	"""
+		This is a test case to check whether an error is returned when an item in a nonexistent wishlist is updated.
+		PUT verb checked here.
+	"""
+
+	def test_update_item_wishlist_not_found(self):
+		new_item = {'description': 'test update'}
+		data = json.dumps(new_item)
+		resp = self.app.put('/wishlists/2/items/item1', data=data, content_type='application/json')
+		self.assertEqual( resp.status_code, status.HTTP_404_NOT_FOUND )
+
+	"""
+		This is a test case to check whether a wishlist is cleared.
+		PUT verb checked here.
+	"""
+
+	def test_clear_wishlist(self):
+		resp = self.app.put('/wishlists/1/items/clear', content_type='application/json')
+		self.assertEqual( resp.status_code, status.HTTP_200_OK )
+		new_json = json.loads(resp.data)
+		self.assertEqual ( len(new_json['items']), 0 )
+
+	"""
+		This is a test case to check whether an error is returned when a nonexistent wishlist is cleared.
+		PUT verb checked here.
+	"""
+
+	def test_clear_wishlist_not_found(self):
+		resp = self.app.put('/wishlists/2/items/clear', content_type='application/json')
 		self.assertEqual( resp.status_code, status.HTTP_404_NOT_FOUND )
 
 
