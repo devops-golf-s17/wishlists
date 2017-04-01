3b26b23f289d63a82108d17a0d8c253173f7a963
zhi cao <zc1087@nyu.edu>
2017-04-01 14:15:37 -0400

    template

--- test_wishlists.py
+++ test_wishlists.py
@@ -25,36 +25,36 @@ class WishlistTestCase(unittest.TestCase):
 		self.assertTrue(len(resp.data) > 0)
 
 	def test_read_wishlist(self):
-		resp = self.app.get('/wishlists/wl1')
+		resp = self.app.get('/wishlists/1')
 		self.assertEqual(resp.status_code, status.HTTP_200_OK)
 		data = json.loads(resp.data)
 		self.assertEqual(data['user_id'], 'palak')
 
 	def test_read_wishlist_not_found(self):
-		resp = self.app.get('/wishlists/wl2')
+		resp = self.app.get('/wishlists/2')
 		self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
 	def test_item(self):
-		resp = self.app.get('/wishlists/wl1/items')
+		resp = self.app.get('/wishlists/1/items')
 		self.assertEqual(resp.status_code, status.HTTP_200_OK)
 		self.assertTrue(len(resp.data) > 0)
 
 	def test_item_not_found(self):
-		resp = self.app.get('/wishlists/wl2/items')
+		resp = self.app.get('/wishlists/2/items')
 		self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
 	def test_read_wishlist_item(self):
-		resp = self.app.get('/wishlists/wl1/items/item1')
+		resp = self.app.get('/wishlists/1/items/item1')
 		self.assertEqual(resp.status_code, status.HTTP_200_OK)
 		data = json.loads(resp.data)
 		self.assertEqual(data['description'], 'test item 1')
 
 	def test_read_wishlist_item_item_not_found(self):
-		resp = self.app.get('/wishlists/wl1/items/item2')
+		resp = self.app.get('/wishlists/1/items/item2')
 		self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
 	def test_read_wishlist_item_wishlist_not_found(self):
-		resp = self.app.get('/wishlists/wl2/items/item2')
+		resp = self.app.get('/wishlists/2/items/item2')
 		self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
 if __name__ == '__main__':
