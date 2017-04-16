7f466e1b7199909c4fc7791bad7c1c5a56b36671
Ankit <ankitbhagat20101994@gmail.com>
2017-04-16 19:47:14 -0400

    Models and Servers file updated but not working yet

--- app/models.py
+++ app/models.py
@@ -2,8 +2,9 @@ import pickle
 from flask import url_for
 from werkzeug.exceptions import NotFound
 from custom_exceptions import DataValidationError
-
+import json
 from datetime import datetime
+import pickle
 ######################################################################
 # Wishlist Model for database
 #   This class must be initialized with use_db(redis) before using
@@ -13,27 +14,41 @@ class Wishlist(object):
     __redis = None
     UPDATABLE_WISHLIST_FIELDS = ['user_id', 'name']
     UPDATABLE_ITEM_FIELDS = ['description']
-
     def __init__(self,id=0,name=None,user_id=None):
         """
         Initializes the internal store of wishlist resources.
         """
         self.id = int(id)
         self.name = name
-        self.user_id = str(user_id)
         self.created = str(datetime.utcnow())
+        self.user_id = str(user_id)
+        self.items = None
         self.deleted = False
-        self.items = {}
 
     def self_url(self):
         return url_for('read_wishlist', wishlist_id=self.id, _external=True)
 
-    def save(self):
-        if self.name==None:
-            raise AttributeError('Name attribute not set!')
+    def save_wishlist(self):
         if self.id==0:
             self.id = self.__next_index()
         Wishlist.__redis.set(self.id, pickle.dumps(self.serialize_wishlist()))
+
+    def save_item(self,data):
+        print "Saving item"
+        if data.get('id')==None:
+            raise AttributeError('ID attribute not set!')
+        if data.get('description')==None:
+            raise AttributeError('Description attribute not set')
+        item_id = data.get('id')
+        item_description = data.get('description')
+        wl_serialized = self.serialize_wishlist()
+        x = wl_serialized['items']
+        if x==None:
+        	x = dict()
+        	x[item_id] = item_description
+        else:
+        	x[item_id] = item_description	
+        Wishlist.__redis.set(self.id, pickle.dumps(self.serialize_wishlist_items(x)))   
 
     def delete(self):
         Wishlist.__redis.delete(self._index)
@@ -43,6 +58,19 @@ class Wishlist(object):
 
     def serialize_wishlist(self):
         return {"id":self.id, "user_id":self.user_id, "name":self.name, "items":self.items, "created":self.created, "deleted":self.deleted}
+
+    def serialize_wishlist_items(self,item_set):
+    	return {"id":self.id, "user_id":self.user_id, "name":self.name, "items":pickle.dumps(item_set), "created":self.created, "deleted":self.deleted}   
+
+    def deserialize_wishlist_items(self,item_set):
+        print "Deserialize items"
+        try:
+            self.items = pickle.dumps(item_set)
+        except KeyError as ke:
+            raise DataValidationError('Invalid wishlist: missing ' + ke.args[0])
+        except TypeError as te:
+            raise DataValidationError('Invalid wishlist: body of request contained bad or no data')
+        return self        
 
     def deserialize_wishlist(self, data):
         try:
@@ -78,8 +106,12 @@ class Wishlist(object):
 
     @staticmethod
     def find(id):
+    	print "find"
         if Wishlist.__redis.exists(id):
             data = pickle.loads(Wishlist.__redis.get(id))
+            print "DATA"
+            print data
+            print Wishlist(data['id'])
             wl = Wishlist(data['id']).deserialize_wishlist(data)
             return wl
         else:
