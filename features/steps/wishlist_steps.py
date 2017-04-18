78361401135e4b4aea48109978fa66a484c17fab
Yuchang Chen <yc1181@nyu.edu>
2017-04-18 19:58:04 -0400

    Update wishlist_steps.py

--- features/steps/wishlist_steps.py
+++ features/steps/wishlist_steps.py
@@ -42,6 +42,11 @@ def step_impl(context, url, id):
     context.resp = context.app.get(target_url)
     assert context.resp.status_code == 200
 
+@then(u'I should see a wishlist with id "{id}"')
+def step_impl(context, id):
+    assert id in context.resp.data
+
+
 @then(u'I should see "{message}" in this wishlist')
 def step_impl(context, message):
     assert message in context.resp.data
