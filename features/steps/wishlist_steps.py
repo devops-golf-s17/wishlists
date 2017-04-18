bde5832273529bd1f0735d525ce29c5a9d441b00
Yuchang Chen <yc1181@nyu.edu>
2017-04-18 19:09:24 -0400

    Update the bdd scenario for get a wishlist with given id

--- features/steps/wishlist_steps.py
+++ features/steps/wishlist_steps.py
@@ -38,3 +38,13 @@ def step_impl(context, url):
 @then(u'I should see a wishlist with id "{id}"')
 def step_impl(context, id):
     assert id in context.resp.data
+
+@when(u'I retrieve "{url}" with id "{id}"')
+def step_impl(context, url, id):
+    target_url = '/{}/{}'.format(url, id)
+    context.resp = context.app.get(target_url)
+    assert context.resp.status_code == 200
+
+@then(u'I should see "{message}" in this wishlist')
+def step_impl(context, message):
+    assert message in context.resp.data
--- features/wishlists.feature
+++ features/wishlists.feature
@@ -30,5 +30,9 @@ Scenario: List all wishlists
 	Then I should see a wishlist with id "1"
 	And I should see a wishlist with id "2"
 	And I should see a wishlist with id "3"
-	
+
+Scenario: Get a wishlist with given id
+    when I retrieve "wishlists" with id "1"
+    Then I should see "user1" in this wishlist
+    Then I should not see "404 Not Found"
 
